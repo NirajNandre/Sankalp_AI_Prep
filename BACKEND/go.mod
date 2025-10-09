b3c7a1cccdcbcd051843d869b32c74cc0b037766
Lakshay Kadam <lakshaykadam@google.com>
2025-10-09 09:54:38 +0000

    go removed , User and Content Models added

--- BACKEND/go.mod
+++
@@ -1,3 +0,0 @@
-module sankalp_api
-
-go 1.25.1
---
+++ BACKEND/sankalp/src/test/java/com/example/sankalp/SankalpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sankalp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SankalpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
